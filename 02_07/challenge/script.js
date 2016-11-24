7f130068664ea4747868bf89feac9e13016cebc2
Thennarasan Shanmugam <sthennarasan@users.noreply.github.com>
2016-11-24 23:29:37 +0530

    Add files via upload

--- 02_07/challenge/script.js
+++ 02_07/challenge/script.js
@@ -4,3 +4,19 @@ var socialMedia = {
   flickr: 'http://flickr.com/planetotheweb',
   youtube: 'http://youtube.com/planetoftheweb'
 };
+
+var socialList = function() {
+  var  output = '<ul>', 
+    myList = document.querySelectorAll('.socialmediaicons');
+
+  for (var key in arguments[0]) {
+    output+= '<li><a href="' + socialMedia[key] + '">' +
+      '<img src="images/' + key + '.png" alt="icon for '+key+'">' +
+      '</a></li>';
+  }
+  output+= '</ul>';
+  
+  for (var i = myList.length - 1; i >= 0; i--) {
+    myList[i].innerHTML = output;
+  };
+}(socialMedia);
